3998d44cebd3b199212ddc0458da40647e92d942
x35yao <46012279+x35yao@users.noreply.github.com>
2019-11-27 23:28:14 -0500

    Update grab_screen.py

--- grab_screen.py
+++ grab_screen.py
@@ -2,14 +2,21 @@ import numpy as np
 from PIL import ImageGrab
 import cv2
 import time
+
+def process_img(original_img):
+    processed_img = cv2.cvtColor(original_img,cv2.COLOR_BGR2GRAY)
+    processed_img = cv2.Canny(processed_img, threshold1 = 200, threshold2 = 300)
+    return processed_img
 last_time = time.time()
 while(True):
     # window size 800*640
     screen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)))
+    new_screen = process_img(screen)
     # printscreen_numpy =  np.array(printscreen_pil.getdata(),dtype='uint8') 
     print('loop took {} seconds'.format(time.time()-last_time))
     last_time = time.time()
-    cv2.imshow('window',cv2.cvtColor(np.array(screen),cv2.COLOR_BGR2RGB))
+    cv2.imshow('window', new_screen)
+    # cv2.imshow('window2',cv2.cvtColor(np.array(screen),cv2.COLOR_BGR2RGB))
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
